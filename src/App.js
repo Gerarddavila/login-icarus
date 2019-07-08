54a627d7f29c0eac5b80a62602d32164ccc07db1
gerarddavila <gerarddavila@gmail.com>
2019-07-07 23:11:57 -0600

    Agregando Campos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form action="">
+      <div className="input-gruop mb-3">
+        <div className="input-group-append">
+          <span className="input-group-text">
+            <i />
+          </span>
+        </div>
+        <input
+          type="text"
+          name=""
+          className="form-control input_user"
+          value=""
+          placeholder="username"
+        />
+      </div>
+    </form>
   );
 }
 
